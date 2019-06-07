a41a5fd89e9acb33c748878350b63f74daff6a48
Zachary Zulanas <zzulanas@gmail.com>
2019-06-06 22:52:00 -0700

    chapters page

---
+++ assets/js/slick.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slideshow').slick({
+        slidesToShow: 1,
+        dots: false,
+        autoplay: true,
+        fade: false,
+        arrows: false,
+        infinite: true,
+        cssEase: 'linear',
+        slidesToScroll: 1,
+        autoplaySpeed:2000,
+     });
+});
