ea98b49c367f19a8ea72e2b6febc5fcf1c405b9c
Zachary Zulanas <zzulanas@gmail.com>
2019-07-04 00:37:46 -0700

    headshots added

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -1,41 +1,23 @@
-var modal = new tingle.modal({
-    footer: true,
+var greta= new tingle.modal({
+    footer: false,
     stickyFooter: false,
     closeMethods: ['overlay', 'button', 'escape'],
     closeLabel: "Close",
     cssClass: ['tingle-modal'],
-    onOpen: function() {
-        console.log('modal open');
-    },
-    onClose: function() {
-        console.log('modal closed');
-    },
-    beforeClose: function() {
-        // here's goes some logic
-        // e.g. save content before closing the modal
-        return true; // close the modal
-        return false; // nothing happens
-    }
 });
 
 // set content
-modal.setContent('<h1 style="font-family:Montserrat; font-size:3em;" class="center">Sign up for Zero Hour\'s Youth Climate Summit now!</h1>');
+greta.setContent('<h1 style="font-family:Montserrat; font-size:3em;" class="red uppercase">Greta Thunberg</h1> <p></p>');
 
-// add a button
-modal.addFooterBtn('Yes please!', 'tingle-btn tingle-btn--primary', function() {
-    openInNewTab('https://bit.ly/zerohourregister')
+var gina= new tingle.modal({
+    footer: false,
+    stickyFooter: false,
+    closeMethods: ['overlay', 'button', 'escape'],
+    closeLabel: "Close",
+    cssClass: ['tingle-modal'],
 });
 
-// add another button
-modal.addFooterBtn('No thanks :(', 'tingle-btn tingle-btn--danger', function() {
-    // here goes some logic
-    modal.close();
-});
+// set content
+gina.setContent('<h1 style="font-family:Montserrat; font-size:3em;" class="red uppercase">Gina McCarthy</h1> <p>Gina McCarthy is Professor of the Practice of Public Health in the Department of Environmental Health at Harvard T.H. Chan School of Public Health and the Director of the Center for Climate, Health, and the Global Environment (Harvard C-CHANGE). In this capacity, she leads the development of the School’s strategy in climate science, health, and sustainability; strengthens the climate science and health curriculum; and liaises with climate science leaders across the University.</p><p>McCarthy has been a leading advocate for common sense strategies to protect public health and the environment for more than 30 years. She served under President Barack Obama as the 13th Administrator of the EPA from 2013–2017. Her tenure as EPA Administrator heralded a paradigm shift in national environmental policy, expressly linking it with global public health. She led EPA initiatives that cut air pollution, protected water resources, reduced greenhouse gases, and strengthened chemical safety to better protect more Americans, especially the most vulnerable, from negative health impacts. McCarthy signed the Clean Power Plan, which set the first-ever national standards for reducing carbon emissions from existing power plants, underscoring the country’s commitment to domestic climate action and spurring international efforts that helped secure the Paris Climate Agreement. McCarthy worked with the United Nations and the World Health Organization on a variety of efforts and represented the U.S. on global initiatives to reduce high-risk sources of pollution.</p><p>A longtime public servant, McCarthy was previously Assistant Administrator for the EPA Office of Air and Radiation, Commissioner of the Connecticut Department of Environmental Protection, Deputy Secretary of the Massachusetts Office of Commonwealth Development, and Undersecretary of Policy for the Massachusetts Executive Office of Environmental Affairs. In the EPA Office of Air and Radiation, McCarthy strengthened collaborative efforts with public health agencies and organizations across the U.S. to identify and reduce threats to human health from harmful air pollution, including carbon pollution that fuels climate change, by updating health and technology based emissions standards, establishing greenhouse gas standards for cars and trucks, promoting energy efficiency and alternative fuels, and mitigating harmful exposures to indoor air pollution. In Connecticut, she was instrumental in developing the Regional Greenhouse Gas Initiative, a multistate effort to reduce emissions contributing to global warming, which has spurred economic growth, improved public health, decreased energy demand and helped mitigate electricity price increases across the region. During her career in Massachusetts, McCarthy advised five governors on environmental affairs, worked at the state and local levels on critical environmental issues, and helped coordinate policies on economic growth, energy, transportation, and the environment.</p><p>After the Obama Administration, McCarthy became a Senior Leadership Fellow at both the Harvard Kennedy School of Government and Harvard T.H. Chan School of Public Health. In addition to her work at Harvard, McCarthy serves on the boards of the Energy Foundation and CERES and is Operating Advisor at Pegasus Capital Advisors, a private equity firm in New York focused on the intersection of global sustainability, health and wellness.<br/>She holds a Master of Science in Environmental Health Engineering and Planning and Policy from Tufts University and a Bachelor of Arts in Social Anthropology from University of Massachusetts at Boston.</p>');
 
-// open modal
-modal.open();
 
-function openInNewTab(url) {
-    var win = window.open(url, '_blank');
-    win.focus();
-  }
